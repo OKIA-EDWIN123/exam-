efcad3827c501d56ec54aa2b92b9ba7099eb5097
OKIA-EDWIN123 <lakrimkareem@gmail.com>
2025-12-06 21:18:20 +0300

    wed site front-end

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
+
+export default {
+  plugins: {
+    tailwindcss: {},  
+    autoprefixer: {}, 
+ },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ucu: {
+          maroon: "#800000",
+          gold: "#FFCC00",
+          dark: "#1A1A1A",
+          light: "#F8F8F8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
